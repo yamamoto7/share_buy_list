53d61fec3a4c9761ec1859867ec83ba33f920f24
kenta.yamamomo <yamamoto7.pgming@gmail.com>
2022-02-20 04:07:08 +0900

    :sparkles: feature: add new screen

---
+++ lib/view/config_screen.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class ConfigScreen extends StatefulWidget {
+  const ConfigScreen({Key? key}) : super(key: key);
+
+  @override
+  _ConfigScreenState createState() => _ConfigScreenState();
+}
+
+class _ConfigScreenState extends State<ConfigScreen> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      alignment: Alignment.center,
+      child: Text("place holder"),
+    );
+  }
+}
---
+++ lib/view/show_group_items_screen.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class ShowGroupItemsScreen extends StatefulWidget {
+  const ShowGroupItemsScreen({Key? key}) : super(key: key);
+
+  @override
+  _ShowGroupItemsScreenState createState() => _ShowGroupItemsScreenState();
+}
+
+class _ShowGroupItemsScreenState extends State<ShowGroupItemsScreen> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      alignment: Alignment.center,
+      child: Text("place holder"),
+    );
+  }
+}
