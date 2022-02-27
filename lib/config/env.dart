28d95775084f4b30e51d054dd45d24d6278c2d36
kenta.yamamomo <yamamoto7.pgming@gmail.com>
2022-02-27 23:15:39 +0900

    :sparkles: feature: env variables

---
+++ lib/config/env.dart
@@ -0,0 +1,6 @@
+import 'package:share_buy_list/config/env_secret.dart';
+
+const int MAX_GOODS_GROUP_NUM = 5;
+const String GRAPHQL_HOST_HTTP = GRAPHQL_HOST_HTTP_SECRET;
+const String GRAPHQL_HOST_WS = GRAPHQL_HOST_WS_SECRET;
+const String GRAPHQL_TOKEN = GRAPHQL_TOKEN_SECRET;
